964825af617667e07cb79f874689bd2a08d0d713
guzhongren <guzhongren@live.cn>
2021-05-09 12:57:31 +0800

    feat(error): error handler

--- exercises/error_handling/result1.rs
+++ exercises/error_handling/result1.rs
@@ -14,6 +14,12 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        if (value < 0) {
+
+            Err(CreationError::Negative)
+        } if (value === 0) {
+            Err(CreationError::Zero)
+        }
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }

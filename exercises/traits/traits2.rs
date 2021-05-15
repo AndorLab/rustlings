a3263d32401c4f60e96d2a567cfb3a8dc96529ad
guzhongren <guzhongren@live.cn>
2021-05-15 19:41:31 +0800

    feat(trait): finish

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,10 +10,15 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
+}
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut v = self;
+        v.push(String::from("Bar"));
+        v
+    }
 }
 
 //TODO: Add your code here
